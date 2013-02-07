a4045cc00646130378fa67ecb7307a61ce818c68
David Muñoz Fernández <shaddow.es@gmail.com>
2013-02-07 23:28:01 +0100

    Implementada la demo

--- Attribute/AttributeViewController.m
+++ Attribute/AttributeViewController.m
@@ -9,21 +9,91 @@
 #import "AttributeViewController.h"
 
 @interface AttributeViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIStepper *selectedWordStepper;
+@property (weak, nonatomic) IBOutlet UILabel *selectedWordLabel;
 @end
 
 @implementation AttributeViewController
 
-- (void)viewDidLoad
+- (void)addLabelAttributes:(NSDictionary *)attributes range:(NSRange)range
+{
+    if (range.location != NSNotFound) {
+        NSMutableAttributedString *mat = [self.label.attributedText mutableCopy];
+        [mat addAttributes:attributes range:range];
+        self.label.attributedText = mat;
+    }
+}
+
+- (void)addSelectedWordAttributes:(NSDictionary *)attributes
+{
+    NSRange range = [[self.label.attributedText string] rangeOfString:[self selectedWord]];
+    [self addLabelAttributes:attributes range:range];
+}
+
+- (IBAction)underline
+{
+    [self addSelectedWordAttributes:@{ NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle)}];
+}
+
+- (IBAction)unUnderline
+{
+    [self addSelectedWordAttributes:@{ NSUnderlineStyleAttributeName : @(NSUnderlineStyleNone)}];
+}
+
+- (IBAction)outline
+{
+    [self addSelectedWordAttributes:@{ NSStrikethroughStyleAttributeName : @(NSUnderlineStyleSingle)}];
+}
+
+- (IBAction)unOutline
+{
+    [self addSelectedWordAttributes:@{ NSStrikethroughStyleAttributeName : @(NSUnderlineStyleNone)}];
+}
+
+- (IBAction)changeColor:(UIButton *)sender
+{
+    [self addSelectedWordAttributes:@{ NSForegroundColorAttributeName : sender.backgroundColor }];
+}
+
+- (IBAction)changeFont:(UIButton *)sender
+{
+    CGFloat fontSize = [UIFont systemFontSize];
+    
+    NSDictionary *attributes = [self.label.attributedText attributesAtIndex:0 effectiveRange:NULL];
+    UIFont *existingFont = attributes[NSFontAttributeName];
+    if (existingFont) {
+        fontSize = existingFont.pointSize;
+    }
+    
+    UIFont *font = [sender.titleLabel.font fontWithSize:fontSize];
+    [self addSelectedWordAttributes:@{ NSFontAttributeName : font}];
+}
+
+- (NSArray *)wordList
+{
+    NSArray *wordList = [[self.label.attributedText string] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if (wordList.count) {
+        return wordList;
+    } else {
+        return @[@""];
+    }
+}
+
+- (NSString *)selectedWord
+{
+    return [self wordList][(int) self.selectedWordStepper.value];
+}
+
+- (IBAction)updateSelectedWord
+{
+    self.selectedWordStepper.maximumValue = [[self wordList] count]-1;
+    self.selectedWordLabel.text = [self selectedWord];
+}
+
+- (void) viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self updateSelectedWord];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
