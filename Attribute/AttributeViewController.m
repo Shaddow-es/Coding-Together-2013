c25c60a88217a5eee12b3f3da4385c08194b84cc
David Muñoz Fernández <shaddow.es@gmail.com>
2013-02-07 22:37:45 +0100

    Proyecto attribute Proyecto de ejemplo con el que empieza la demo de los attributed string

---
+++ Attribute/AttributeViewController.m
@@ -0,0 +1,29 @@
+//
+//  AttributeViewController.m
+//  Attribute
+//
+//  Created by David Muñoz Fernández on 07/02/13.
+//  Copyright (c) 2013 David Muñoz Fernández. All rights reserved.
+//
+
+#import "AttributeViewController.h"
+
+@interface AttributeViewController ()
+
+@end
+
+@implementation AttributeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
