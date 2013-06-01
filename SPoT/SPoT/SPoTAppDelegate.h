1f0025709115b0129bc7e88ff823dab9c71a7cd9
David Muñoz Fernández <shaddow.es@gmail.com>
2013-06-01 21:10:24 +0200

    Añadido proyecto SPoT

---
+++ SPoT/SPoT/SPoTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPoTAppDelegate.h
+//  SPoT
+//
+//  Created by David Muñoz Fernández on 16/05/13.
+//  Copyright (c) 2013 David Muñoz Fernández. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPoTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
